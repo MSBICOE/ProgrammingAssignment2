761113bfbcd59c89601318795f894b351caa079e
MSBICOE <shangzhouwang@hotmail.com>
2014-06-22 23:44:44 +1000

    made the change to cacheSolve to take care of non-square and for those matrices its determinants are 0s.

--- cachematrix.R
+++ cachematrix.R
@@ -14,7 +14,10 @@ makeCacheMatrix <- function(x = matrix())
                 m <<- NULL		# Invalidate the cached matrix				
         }
         get <- function() { x }
-        setInverse <- function (func)	m <<- func
+        setInverse <- function (func)	
+        {
+                m <<- func		#Recalculating the based on the func variable
+        }
         getInverse <- function() m
         list	(	set = set
                , get = get
@@ -29,6 +32,7 @@ makeCacheMatrix <- function(x = matrix())
 cacheSolve <- function(x, ...) 
 {
         ## Return a matrix that is the inverse of 'x'
+        #x <- makeCacheMatrix(x)
         m <- x$getInverse()
         if (!is.null(m))
         {
@@ -36,6 +40,36 @@ cacheSolve <- function(x, ...)
                 return(m)			
         }
         newMatrix <- x$get()
+        # check to see if the matrix is invertible
+        myRow <- nrow(newMatrix)
+        myCol <- ncol(newMatrix)
+        # First to check if a matrix is a square one
+        if (myRow != myCol)
+        {
+                message("The matrix is not invertible")
+                m <- NA
+                return(m)
+        }
+        # Second to check if the determinant of the matrix is 0
+        comboMatrix <- cbind(newMatrix, newMatrix[,1:(myCol - 1)])	# combine the matrix to the left except the last column
+        myint <- rep(1,length.out = (2 * myRow))	# initiate a vector value with 1 twice size as matrix rows
+        myD <-data.frame(myint)
+        for (i in 1:myRow)
+        {
+                for (j in 1:myCol)
+                {
+                        myD[j,] <- comboMatrix[i,i + j - 1] * myD[j,]	# calculate downwards diagonal value 
+                        myD[j + myRow,] <- comboMatrix[myRow - i + 1, i + j - 1] * myD[j + myRow,]	# calculate upwards diagonal value 
+                }
+        }
+        mydet <- sum(head(myD,myRow)) - sum(tail(myD,myRow))   # calculate the determinant
+        if (mydet == 0)
+        {
+                message("The determinant of the matrix is 0 so the matrix is not invertible")
+                m <- NA
+                return(m)
+        }
+        # if not cached and it is invertible then do the inverse calculation
         m <- solve(newMatrix,...)
         x$setInverse(m)
         m
