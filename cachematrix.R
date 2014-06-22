c13793c93cc710d69354c9573e949507f2f389d6
MSBICOE <shangzhouwang@hotmail.com>
2014-06-22 14:52:09 +1000

    First version of assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,17 +3,37 @@
 ##Date: 19th Jun 2014
 ############################################################
 
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+        m <- NULL		# do not assign anything to m (using cached value if any)
+        set <- function (y)
+        {
+                x <<- y
+                m <<- NULL		# Invalidate the cached matrix				
+        }
+        get <- function() { x }
+        setInverse <- function (solve)	m <<- solve
+        getInverse <- function() m
+        list	(	set = set
+               , get = get
+               , setInverse = setInverse
+               , getInverse = getInverse
+        )				
 }
 
-
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse
+        if (!is.null(m))
+        {
+                message("Getting the inverse matrix from cache")
+                return(m)			
+        }
+        newMatrix <- x$get()
+        m <- solve(newMatrix,...)
+        x$setInverse(m)
+        m
 }
